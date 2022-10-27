0739730291366495ede2099e6674c7574ae4ef87
Thomas Huisman <contact@thohui.com>
2022-10-27 04:30:19 +0200

    update models

--- models/user.go
+++ models/user.go
@@ -11,12 +11,16 @@ type Provider struct {
 }
 
 type User struct {
-	ID            string  `json:"id" gorm:"primaryKey"`
-	Name          string  `json:"name" gorm:"not null"`
-	Role          int     `json:"role" gorm:"not null"`
-	Password      *string `json:"-"`
-	Email         string  `json:"email" gorm:"not null"`
-	EmailVerified bool    `json:"email_verified" gorm:"not null"`
-	TokenVersion  int     `json:"token_version" gorm:"default:1"`
-	Updated       int64   `gorm:"autoUpdateTime:milli"` // Use unix milli seconds as updating time
+	ID           string  `json:"id" gorm:"primaryKey"`
+	Name         string  `json:"name" gorm:"not null"`
+	Role         int     `json:"role" gorm:"not null"`
+	Password     *string `json:"-"`
+	Email        string  `json:"email" gorm:"not null"`
+	TokenVersion int     `json:"token_version" gorm:"default:1"`
+	Updated      int64   `gorm:"autoUpdateTime:milli"`
+}
+type AccountVerification struct {
+	Email            string
+	VerificationCode string
+	ExpiresAt        int64
 }
